3dbbfcdbf0be9ee28fd06deb3169551232848d6e
EroshVK <635753051vika@gmai.com>
2023-05-22 21:01:35 +0300

    add class and test from HW5

--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -1,1 +1,17 @@
-"""Здесь надо написать тесты с использованием unittest для модуля linked_list."""
+import unittest
+
+from src.linked_list import LinkedList
+
+
+class TestLinkedList(unittest.TestCase):
+    def test_linked_list(self):
+        ll = LinkedList()
+        ll.insert_beginning({'id': 1})
+        ll.insert_at_end({'id': 2})
+        ll.insert_at_end({'id': 3})
+        ll.insert_beginning({'id': 0})
+        self.assertEqual(str(ll), "{'id': 0} -> {'id': 1} -> {'id': 2} -> {'id': 3} -> None")
+
+
+if __name__ == "__main__":
+    unittest.main()
